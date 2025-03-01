15423c8a72b8353772ea54e085c5ac440ba01d45
GurusaiBiradar <gurusaibiradar@gmail.com>
2025-03-01 03:07:17 +0100

    Function to load graphs from JSON

--- graph_search_task.py
+++ graph_search_task.py
@@ -4,7 +4,30 @@
 
 # Note: Available files are: workpiece_graph.json, feature_graph.json
 
-# TODO
+import json
+import networkx as nx
+
+def load_graph_from_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    G = nx.Graph()  # Use nx.DiGraph() if the graphs are directed.
+    
+    # Load nodes from JSON. Each node is given as [id, attributes]
+    for node in data.get("nodes", []):
+        node_id, attrs = node
+        G.add_node(node_id, **attrs)
+    
+    # Load edges from JSON. Each edge is given as [source, target, attributes]
+    for edge in data.get("edges", []):
+        source, target, attrs = edge
+        G.add_edge(source, target, **attrs)
+    
+    return G
+
+# Load both graphs.
+workpiece_graph = load_graph_from_json("workpiece_graph.json")
+feature_graph = load_graph_from_json("feature_graph.json")
+
 
 # ##################################################
 # 2) Create graphs from loaded data
