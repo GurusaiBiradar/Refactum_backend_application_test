68b5c5edb535549d3e76106daea7f4165cdb67fa
GurusaiBiradar <gurusaibiradar@gmail.com>
2025-03-01 03:38:03 +0100

    Definitions to check if feature graph is a subgraph of the workpiece and find matches

--- graph_search_task.py
+++ graph_search_task.py
@@ -1,33 +1,37 @@
+import json
+import networkx as nx
+from networkx.algorithms.isomorphism import GraphMatcher
+
 # ##################################################
 # 1) Load workpiece graph and feature graph data from  json file
 # ##################################################
 
 # Note: Available files are: workpiece_graph.json, feature_graph.json
 
-import json
-import networkx as nx
-
 def load_graph_from_json(file_path):
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-    G = nx.Graph()  # Use nx.DiGraph() if the graphs are directed.
+    with open(file_path, 'r') as file:
+        data = json.load(file)
     
-    # Load nodes from JSON. Each node is given as [id, attributes]
-    for node in data.get("nodes", []):
-        node_id, attrs = node
-        G.add_node(node_id, **attrs)
+    graph = nx.Graph()
     
-    # Load edges from JSON. Each edge is given as [source, target, attributes]
-    for edge in data.get("edges", []):
-        source, target, attrs = edge
-        G.add_edge(source, target, **attrs)
+    # Add nodes
+    for node_entry in data['nodes']:
+        node_id = node_entry[0]
+        attributes = node_entry[1]
+        graph.add_node(node_id, **attributes)
     
-    return G
+    # Add edges
+    for edge_entry in data['edges']:
+        source = edge_entry['source']
+        target = edge_entry['target']
+        attributes = edge_entry['attributes']
+        graph.add_edge(source, target, **attributes)
+    
+    return graph
 
-# Load both graphs.
-workpiece_graph = load_graph_from_json("workpiece_graph.json")
-feature_graph = load_graph_from_json("feature_graph.json")
-
+# Load graphs
+feature_graph = load_graph_from_json('feature_graph.json')
+workpiece_graph = load_graph_from_json('workpiece_graph.json')
 
 # ##################################################
 # 2) Create graphs from loaded data
@@ -49,8 +53,17 @@ feature_graph = load_graph_from_json("feature_graph.json")
 # 3) Check if the feature graph is a subgraph of the workpiece workpiece and find any other matching subgraphs
 # ##################################################
 
+def node_match(node1, node2):
+    return node1.get('type') == node2.get('type')
 
-# TODO
+def edge_match(edge1, edge2):
+    return edge1.get('angular_type') == edge2.get('angular_type')
+
+def find_matching_subgraphs(feature_g, workpiece_g):
+    matcher = GraphMatcher(workpiece_g, feature_g, node_match=node_match, edge_match=edge_match)
+    return list(matcher.subgraph_isomorphisms_iter())
+
+matching_subgraphs = find_matching_subgraphs(feature_graph, workpiece_graph)
 
 # ##################################################
 # 4) Results
